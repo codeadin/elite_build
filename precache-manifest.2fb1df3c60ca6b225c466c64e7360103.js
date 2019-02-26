5c017dabaaa6d6d2c177a87232350796619bba80
codeadin <viking@maggnus.is>
2019-02-26 21:28:51 +0000

    Initial Build
    Seems to be working..

---
+++ precache-manifest.2fb1df3c60ca6b225c466c64e7360103.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ddb0fb335820ba0f94d",
+    "url": "/static/css/main.b3ef6181.chunk.css"
+  },
+  {
+    "revision": "4ddb0fb335820ba0f94d",
+    "url": "/static/js/main.4ddb0fb3.chunk.js"
+  },
+  {
+    "revision": "423acc93c10545aadf04",
+    "url": "/static/js/1.423acc93.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a4daa5ff39f039a43dfb53a83829b52f",
+    "url": "/static/media/logo.a4daa5ff.jpg"
+  },
+  {
+    "revision": "3acb923df749f3948acd89b2d9fca994",
+    "url": "/static/media/Oxararfoss-full.3acb923d.jpeg"
+  },
+  {
+    "revision": "7bd7b76dcea0a05646920fb7504dac92",
+    "url": "/index.html"
+  }
+];
